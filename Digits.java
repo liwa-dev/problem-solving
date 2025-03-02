7c607bda1137719c7ce77885600bb02cd1cfbc26
liwa-dev <strrrtm1@gmail.com>
2025-03-02 13:30:23 +0100

    Digital

--- Digits.java
+++ Digits.java
@@ -4,14 +4,18 @@ public class Digits {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int size = sc.nextInt();
-        
         for (int i = 0; i < size; i++) {
             int value = sc.nextInt(); // 56486
+            if (value == 0) {
+                System.out.println(0);
+                continue;
+            }
             while (value != 0) {
                 int my_num = value % 10; //5648.6
+                System.out.print(my_num  + " ");
                 value = value / 10; // 5648
             }
-
+            System.out.println();
         }
 
     }
